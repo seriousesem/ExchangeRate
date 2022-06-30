551141a99ec8e5eccd8e4a1d234c247d48e33e2c
iSem <semenenko.andrej@gmail.com>
2022-06-30 20:48:55 +0300

    add list currency

--- app/src/main/java/com/serioussem/exchangerate/utils/Constants.kt
+++ app/src/main/java/com/serioussem/exchangerate/utils/Constants.kt
@@ -4,21 +4,44 @@ import com.serioussem.exchangerate.R
 
 object Constants {
     val countryFlag = listOf(
-        R.drawable.us, R.drawable.eur, R.drawable.gb,
-        R.drawable.jp, R.drawable.ch, R.drawable.cn,
+        R.drawable.us, R.drawable.eur, R.drawable.gb, R.drawable.jp, R.drawable.ch,
+        R.drawable.cn,R.drawable.ae,
         R.drawable.noting
     )
     val currencyFullName = listOf(
         R.string.dollar_usa, R.string.euro, R.string.pound_sterling, R.string.japanese_yen,
-        R.string.swiss_franc, R.string.yuan_renminbi, R.string.any_currency, R.string.aoe_dih,
+        R.string.swiss_franc, R.string.yuan_renminbi, R.string.aoe_dih,
         R.string.afgani, R.string.alb_lek, R.string.virm_dram, R.string.ang_kvan, R.string.arg_peso,
         R.string.avstr_dol, R.string.azer_man, R.string.bang_taka, R.string.bolg_lev, R.string.bahr_din,
-        R.string.burun_fran
+        R.string.burun_fran, R.string.brun_dol, R.string.boliv, R.string.braz_real, R.string.botsv_pul,
+        R.string.bilorus_rub, R.string.canad_dol, R.string.kongo_fran, R.string.chili_peso, R.string.kolumb_peso,
+        R.string.kosta_rik_kol, R.string.kuba_peso, R.string.cheh_krona, R.string.frank_jibut, R.string.dania_krona,
+        R.string.aljir_dinar, R.string.egipt_funt, R.string.efiop_bir, R.string.gruzia_lari, R.string.gana_sedi,
+        R.string.gambia_dalasi, R.string.gvinea_frank, R.string.gonkong_dol, R.string.horvat_kuna,
+        R.string.ugorsk_forin, R.string.indonez_rupia, R.string.izrail_shekel, R.string.india_rupia,
+        R.string.irak_dinar, R.string.iran_real, R.string.island_krona, R.string.yordan_dinar, R.string.kenia_shiling,
+        R.string.kirgis_som, R.string.kamboj_riel, R.string.nord_korea_von, R.string.korea_von,
+        R.string.kuvejt_dinar, R.string.kazach_tenge, R.string.laos_kir, R.string.livan_funt,
+        R.string.rupia_shri_lanka, R.string.livia_dinar, R.string.marokko_dichram, R.string.moldova_lei,
+        R.string.madagaskar_ariari, R.string.makedon_denar, R.string.mongol_tugrik, R.string.mavritan_ugia,
+        R.string.mavrikia_rupia, R.string.malavia_kvacha, R.string.mexico_peso, R.string.malazia_ringit,
+        R.string.mozambik_montikal, R.string.namibia_dolar, R.string.nigeria_naira, R.string.nikaragua_kord,
+        R.string.norveg_krona, R.string.nepal_rupia, R.string.novozeland_dolar, R.string.oman_real,
+        R.string.peru_nuevo, R.string.filipin_peso, R.string.pakistan_rupia, R.string.polska_zlot,
+        R.string.paragvaj_guarani, R.string.katar_real, R.string.rumunia_lei, R.string.serbia_dinar,
+        R.string.saudia_real, R.string.seishel_rupia, R.string.sudan_funt, R.string.swed_krona,
+        R.string.singapur_dolar, R.string.sierra_leone, R.string.somali_shiling, R.string.surinam_dolar,
+        R.string.siria_funt, R.string.svazilend_lilang, R.string.tailand_bat, R.string.tadjik_somoni,
+        R.string.turkmen_manat, R.string.tunis_dinar, R.string.turkish_lira, R.string.taivan_dolar,
+        R.string.tanzania_shiling, R.string.uganda_shiling, R.string.uragvai_peso, R.string.uzbek_sum,
+        R.string.venesuel_bolivar, R.string.vietnam_dong, R.string.afric_frank1, R.string.afric_frank2,
+        R.string.emen_real, R.string.lesotto_rand, R.string.zambia_kvacha, R.string.any_currency
     )
+
     val currencyCode =
         listOf(
             840, 978, 826, 392, 756, 156, 784, 971, 8, 51, 973, 32, 36, 944,
-            50, 975, 48, 108   , 96, 68, 986, 72, 933, 124, 976, 152, 170, 188,
+            50, 975, 48, 108, 96, 68, 986, 72, 933, 124, 976, 152, 170, 188,
             192, 203, 262, 208, 12, 818, 230, 981, 936, 270, 324, 344, 191,
             348, 360, 376, 356, 368, 364, 352, 400, 404, 417, 116, 408, 410,
             414, 398, 418, 422, 144, 434, 504, 498, 969, 807, 496, 478, 480,
@@ -27,4 +50,5 @@ object Constants {
             968, 760, 748, 764, 972, 795, 788, 949, 901, 834, 800, 858, 860,
             937, 704, 950, 952, 886, 710, 894
         )
+
 }
